e9e96456c79742ab94b3c7c6ec55e52de9377d35
Atoche Pascual <jmatochepascual@gmail.com>
2023-05-20 14:51:40 -1000

    [ADD] verifica si y exite el curso en el carrito

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const carrito = document.querySelector('#carrito');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const btnVaciarCarrito = document.querySelector('#vaciar-carrito');
 const listaCursos = document.querySelector('#lista-cursos');
+const carritoCursos = [];
 
 
 // * EVENTOS
@@ -18,20 +19,29 @@ const agregarCurso = (event) => {
   event.preventDefault();
 
   if (event.target.classList.contains('agregar-carrito')) {
-    const curso = event.target.parentElement.parentElement;
-    obtenerDatosCursos(curso);
+
+    const cursoHTML = event.target.parentElement.parentElement;
+
+    const cursoObj = obtenerDatosCursos(cursoHTML);
+
+    const existe = existeCursoEnCarritoCursos(cursoObj);
+
   }
 }
 
 //* Obtiene los datos del curso seleccionado
-const obtenerDatosCursos = (curso) => {
+const obtenerDatosCursos = (cursoHTML) => {
   const objetoCurso = {
-    imagen: curso.querySelector('img').src,
-    titulo: curso.querySelector('h4').textContent,
-    precio: curso.querySelector('.precio span').textContent,
-    id: curso.querySelector('a').getAttribute('data-id'),
+    imagen: cursoHTML.querySelector('img').src,
+    titulo: cursoHTML.querySelector('h4').textContent,
+    precio: cursoHTML.querySelector('.precio span').textContent,
+    id: cursoHTML.querySelector('a').getAttribute('data-id'),
     cantidad: 1,
   };
+  return objetoCurso;
+};
 
-  console.log(objetoCurso);
+//* Verifica si ya existe el curso en el carrito de cursos
+const existeCursoEnCarritoCursos = (cursoObj) => {
+  return carritoCursos.some(curso => curso.id === cursoObj.id);
 };
